3c4176de1be153ed310ce6f8014261c6d4bf267b
Grey-Ashh <146908416+Grey-Ashh@users.noreply.github.com>
2023-11-30 14:11:32 +0530

    Update threadexample.java

--- Set_6/threadexample.java
+++ Set_6/threadexample.java
@@ -3,7 +3,12 @@ class odd extends Thread
 	public void run()
 	{
 		for(int i=3;i<100;i=i+2)
-			System.out.print(i+"  ");
+			{
+				System.out.print(i+"  ");
+				try{
+					Thread.sleep(500);
+				}catch(Exception e){System.out.println(e);}
+			}
 	}
 }
 class even extends Thread
@@ -11,14 +16,21 @@ class even extends Thread
 		public void run()
 	{
 		for(int i=2;i<100;i=i+2)
-			System.out.print(i+"  ");
+			{
+				System.out.print(i+"  ");
+				try{
+					Thread.sleep(250);
+				}catch(Exception e){System.out.println(e);}
+			}
 	}
 }
 class threadexample
 {
 	public static void main(String args[])
 		{
-			new odd().start();
-			new even().start();
+			odd o=new odd();
+			even e=new even();
+			e.start();
+			o.start();
 		}
 }
